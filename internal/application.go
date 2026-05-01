276eee59df2cbe317de6cd5c19020ede7cd4044e
KarnaukhovGA69 <glebhleb226228@gmail.com>
2026-05-01 17:13:22 +0300

    add short url algorihtm

--- internal/application.go
+++
@@ -1,7 +0,0 @@
-package internal
-
-import "go.uber.org/zap"
-
-func run(log *zap.Logger, database DB) {
-
-}
--- shorturl/shorturl.go
+++ shorturl/shorturl.go
@@ -1,10 +1,24 @@
 package shorturl
 
+import (
+	"crypto/rand"
+	"math/big"
+)
+
 const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
 const length = 10
 
-func MakeShortURL(url string) string {
-	baseURL := "http://"
-	result := baseURL
-	return result
+func MakeShortURL() (string, error) {
+	result := make([]byte, length)
+
+	for i := 0; i < length; i++ {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(alphabet))))
+		if err != nil {
+			return "", err
+		}
+
+		result[i] = alphabet[n.Int64()]
+	}
+
+	return string(result), nil
 }
