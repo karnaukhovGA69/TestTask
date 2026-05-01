a93abd4362ff5735f7f0b64a2d4315abfa4a7b9b
KarnaukhovGA69 <glebhleb226228@gmail.com>
2026-05-01 20:57:52 +0300

    add postgres and half docker-compose

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.25.1
 
 require go.uber.org/zap v1.28.0
 
-require go.uber.org/multierr v1.10.0 // indirect
+require (
+	github.com/lib/pq v1.12.3 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+)
---
+++ internal/storage/postgres/postgres.go
@@ -0,0 +1,102 @@
+package postgres
+
+import (
+	"database/sql"
+	"errors"
+	"main/shorturl"
+
+	"github.com/lib/pq"
+)
+
+var ErrNotFound = errors.New("ссылка не найдена")
+
+const maxGenerateAttempts = 69
+
+type PostgresDB struct {
+	db *sql.DB
+}
+
+func NewPostgresDB(dsn string) (*PostgresDB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return &PostgresDB{db: db}, nil
+}
+
+func (p *PostgresDB) GetShortURL(url string) (string, error) {
+	var shortURL string
+	err := p.db.QueryRow(`SELECT shortURL FROM urls WHERE longURL = $1`, url).Scan(&shortURL)
+	if err == nil {
+		return shortURL, nil
+	}
+	if errors.Is(err, sql.ErrNoRows) {
+		return "", ErrNotFound
+	}
+	return "", err
+}
+
+func (p *PostgresDB) GetLongURL(url string) (string, error) {
+
+	var longURL string
+	err := p.db.QueryRow(`SELECT longURL FROM urls WHERE shortURL = $1`, url).Scan(&longURL)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return "", ErrNotFound
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	return longURL, nil
+
+}
+
+func (p *PostgresDB) AddURL(url string) (string, error) {
+	var oldShortURL string
+	err := p.db.QueryRow(`SELECT shortURL FROM urls WHERE longURL = $1`, url).Scan(&oldShortURL)
+	if err == nil {
+		return oldShortURL, nil
+	}
+	if !errors.Is(err, sql.ErrNoRows) {
+		return "", err
+	}
+	for i := 0; i < maxGenerateAttempts; i++ {
+		newShortURL, err := shorturl.MakeShortURL()
+		if err != nil {
+			return "", errors.New("Не получилось создать URL")
+		}
+
+		_, err = p.db.Exec(`INSERT INTO urls (longURL, shortURL) VALUES ($1,$2)`, url, newShortURL)
+		if err == nil {
+			return newShortURL, nil
+		}
+		if isUniqueViolation(err) {
+			continue
+		}
+		return "", err
+
+	}
+	return "", errors.New("Не получилось создать короткий URL")
+}
+
+func isUniqueViolation(err error) bool {
+	var pqErr *pq.Error
+
+	if errors.As(err, &pqErr) {
+		return pqErr.Code == "23505"
+	}
+
+	return false
+}
+
+func (p *PostgresDB) Close() error {
+	return p.db.Close()
+}
